aef8e7c1c8b07ab3e2e412af23285b1e3b5d42cf
China-a <China.allford@gmail.com>
2022-05-31 11:20:55 -0400

    Initial commit

---
+++ src/main/java/com/example/springboot_cont_securing_web/SpringBootContSecuringWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot_cont_securing_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootContSecuringWebApplication {
+
+    public static void main(String[] args) throws Throwable {
+        SpringApplication.run(SpringBootContSecuringWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springboot_cont_securing_web/SpringBootContSecuringWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot_cont_securing_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootContSecuringWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
